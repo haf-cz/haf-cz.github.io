a46510d57507b348006039b8b1efb962782be880
Josef Kafka <josefkafka1@gmail.com>
2019-11-20 12:34:53 +0100

    ading debug

--- examples/ballExample/scripts/main.js
+++ examples/ballExample/scripts/main.js
@@ -1,17 +1,22 @@
 // actual position of ball's center
 var pos = {
-	x : 15,
-    y : 15
+	x : 15.0,
+    y : 15.0
 };
 
 // speed vector
 var speed = {
-	x : 0,
-	y : 1
+	x : 0.0,
+	y : 1.0
 };
 
+// external constant/forces etc.
+var ext = {
+	speedIncY : 2.0 // speed increment
+}
+
 var ball = {
-	r : 10,
+	r : 10.0,
 	arcS : 0,
 	arcE : 2 * Math.PI,
 	fillStyle : "#FF0000",
@@ -20,11 +25,25 @@ var ball = {
 
 
 // timer 100ms
-var myTimer100ms = setInterval(update, 100);
+var myTimer100ms;
 var canvas;
+var debug;
+var buttons = {
+	start,
+	pause,
+	step
+};
+var simualtion = false;
 
 function onLoad() {
 	canvas = document.getElementById("myCanvas");
+	debug = document.getElementById("debug");
+	
+	buttons.start = document.getElementById("start");
+	buttons.pause = document.getElementById("pause");
+	buttons.step = document.getElementById("step");
+	
+	start();
 }
 
 function resetBall() {
@@ -33,45 +52,90 @@ function resetBall() {
 	pos.y = ball.r + ball.r/2;
 	
 	// reset speed
-	speed.x = 0;
-	speed.y = 1;
+	speed.x = 0.0;
+	speed.y = 15.0;
+	
+	update();
+}
+
+function step() {
+	update();
+}
+
+function start() {
+	simulation(true);
+	// start timer
+	myTimer100ms = setInterval(update, 100);
+}
+
+function pause() {
+	simulation(false);
+	// pause timer
+	clearInterval(myTimer100ms)
+}
+
+function simulation(isRunning) {
+	simualtion = isRunning;
+	
+	buttons.start.disabled = isRunning;
+	buttons.pause.disabled = !isRunning;
+	buttons.step.disabled =  isRunning;
 }
 
 function update() {
   updatePosition();
   redrawCanvas();
+  updateDebug();
 }
 
 function redrawCanvas() {
-var ctx = canvas.getContext("2d");
+	var ctx = canvas.getContext("2d");
 
-// clear drawing area
-ctx.clearRect(0, 0, canvas.width, canvas.height);
+	// clear drawing area
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-ctx.beginPath();
-ctx.arc(pos.x, pos.y, ball.r, ball.arcS, ball.arcE);
-ctx.fillStyle = ball.fillStyle;
-ctx.fill();
-ctx.strokeStyle = ball.strokeStyle;
-ctx.stroke();
+	ctx.beginPath();
+	ctx.arc(pos.x, pos.y, ball.r, ball.arcS, ball.arcE);
+	ctx.fillStyle = ball.fillStyle;
+	ctx.fill();
+	ctx.strokeStyle = ball.strokeStyle;
+	ctx.stroke();
 }
 
-function updatePosition() {
-
-	// check bouncing +-1 for sroke
-	if(pos.y + ball.r + 1 >= canvas.height) {
+function updatePosition() {	
+	// local helper
+	var b = {
+		bottom : pos.y + ball.r + speed.y,
+		top : pos.y - ball.r + speed.y // + speed as speed can be negative
+	};
+	
+	var revertSpeed = false;
+	
+	// check bouncing
+	if(b.bottom >= canvas.height) {
 		// when bottom part of ball hits ground then revert speed
-		speed.y = -speed.y;
+		revertSpeed = true;
 	}
 	
-	if(pos.y - ball.r - 1 <= 0) {
+	if(b.top <= 0) {
 		// when top of ball hits roof then revert speed
+		revertSpeed = true;
+	}
+	
+	if(revertSpeed)
+	{
 		speed.y = -speed.y;
+	}
+	else
+	{
+		// update speed -> linear change
+		speed.y += ext.speedIncY;
 	}
 	
 	// update position
 	pos.y += speed.y;
-	
-	// update speed
-	speed.y += 1;
+}
+
+function updateDebug() {
+	debug.innerHTML = "[" + pos.x + "," + pos.y + "] (" +	speed.x + "," + speed.y + ")";
 }
