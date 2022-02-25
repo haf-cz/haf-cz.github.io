654bd3d37c00e3d0e124d4426f918585c31e54ab
haf-cz <josefkafka1@gmail.com>
2022-02-25 21:29:33 +0100

    Adding keyPressed with teting keys

--- fun/coin-hunter/main.js
+++ fun/coin-hunter/main.js
@@ -14,10 +14,42 @@ const playerPos = {
 
 let player = document.getElementById("panacek");
 
+const spaceBar = " ";
+const arrowLeft = "ArrowLeft";
+const arrowUp = "ArrowUp";
+const arrowRight = "ArrowRight";
+const arrowDown = "ArrowDown";
+
 startGame();
 
 function startGame() {
 	console.log("start");
 	player.style.top = playerPos.Y + "px";
 	player.style.left = playerPos.X + "px";
+}
+
+function keyPressed(event) {
+	let top = parseInt(player.style.top);
+	let left = parseInt(player.style.left);
+	const dx = 5;
+	const dy = 5;
+
+	switch (event.key)
+	{
+		case spaceBar:
+			startGame();
+			break;
+		case arrowDown:
+			top += dy;
+			break;
+		case arrowUp:
+			top -= dy;
+			break;
+		case arrowRight:
+			left += dx;
+			break;
+		case arrowLeft:
+			left -= dx;
+			break;
+	}
 }
